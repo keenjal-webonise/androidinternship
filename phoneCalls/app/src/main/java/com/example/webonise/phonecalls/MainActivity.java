9dadd67d38de844b712f2c5596b6365a23b4cdd0
keenjal-webonise <keenjal.modi@weboniselab.com>
2016-06-15 17:39:45 +0530

    file add phoneCalls.java

---
+++ phoneCalls/app/src/main/java/com/example/webonise/phonecalls/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.webonise.phonecalls;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        call();
+    }
+
+    private void call() {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("7383888443"));
+        intent.setData(Uri.parse("tel:7383888443"));
+        try{
+            startActivity(intent);
+        }
+
+        catch (android.content.ActivityNotFoundException ex){
+            Toast.makeText(getApplicationContext(),"yourActivity is not founded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
